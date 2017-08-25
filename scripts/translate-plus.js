b6a783d50f7367d4f60ae4eda5c226410f8f34b9
picasso <picasso@picasso-italy.local>
2017-08-25 16:22:33 +0200

    added folders & .gitattributes

---
+++ scripts/translate-plus.js
@@ -0,0 +1,13 @@
+(function($) {
+
+	$(document).ready( function() {
+	  
+		var 	//$window = $(window), 				// Cache selectors
+				$body = $('body');
+
+		$body.hasClass('translate-plus');
+		
+	}); // end of $(document).ready
+
+})(jQuery);
+
