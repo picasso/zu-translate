eb5472a08606cc02d80e2a7ef63cd488017d5c1c
picasso <dmitry.rudakov@icloud.com>
2020-11-04 14:14:12 +0100

    fixed bugs in menu reorder, added 'from_split_index' and 'from_moved_index' methods

--- traits/admin-menu.php
+++ traits/admin-menu.php
@@ -4,10 +4,13 @@
 
 trait zukit_AdminMenu {
 
-	protected $default_menu_id = 'options-general.php';
-	protected $menu_split_id = 'options-privacy.php';
+	private static $default_menu_id = 'options-general.php';
+	private static $menu_split_id = 'options-privacy.php';
+	private static $menu_split_index = null;
+	private static $menu_moved_index = null;
+
+	private $shift_gap = 20;
 	protected $total_index_break = 220;		// max items count in submenu?
-	protected $menu_split_index = 0;
 
 	public function admin_menu_config() {
 		add_filter('custom_menu_order', [$this, 'admin_menu_modify']);
@@ -22,100 +25,117 @@ trait zukit_AdminMenu {
 	//		'before_index'		- item position will be before item with this slug
 	//		'before_index2'		- item position will be before item with this slug - 1 (the space could be used for separator later)
 	//		'new_name'			- new item name
-	//		'parent'			- parent menu slug (if absent then  'options-general.php' will be used)
+	//		'parent'			- parent menu slug (if absent then 'options-general.php' will be used)
 
 	protected function custom_admin_menu() { return []; }
 	protected function custom_admin_submenu() { return []; }
 
 	// Admin menu modify ------------------------------------------------------]
 
+	private function get_split_index() {
+		global $submenu;
+		if(empty(self::$menu_split_index)) {
+			if(!isset($submenu[self::$default_menu_id])) return null;
+
+			$submenu_items = count($submenu[self::$default_menu_id]);
+			self::$menu_split_index = $this->get_submenu_index(self::$menu_split_id);
+			$split_position = array_search(self::$menu_split_index, array_keys($submenu[self::$default_menu_id]));
+			$moved_items = $submenu_items - $split_position - 1;
+
+			// clean positions from $menu_split_index + 1
+			$this->submenu_move(
+				self::$menu_split_index + 1,
+				self::$menu_split_index + $moved_items + $this->shift_gap + 1,
+				$moved_items
+			);
+		}
+		return true;
+	}
+
+	public function from_split_index($shift = 0) {
+		return self::$menu_split_index + $shift;
+	}
+
+	public function from_moved_index($shift = 0) {
+		return self::$menu_moved_index + $shift;
+	}
+
 	public function admin_menu_modify($menu_order) {
 	    global $menu, $submenu;
 
-		$default_menu_id = $this->default_menu_id;
-		$menu_split_id = $this->menu_split_id ;
-		// _dbug($submenu[$default_menu_id]);
-
-		if(empty($this->menu_split_index)) {
-			$menu_id = $default_menu_id;
-
-			if(!isset($submenu[$menu_id])) return $menu_order;
-
-			$submenu_items = count($submenu[$menu_id]);
-			$this->menu_split_index = $this->get_submenu_index($menu_split_id);
-			$split_position = array_search($this->menu_split_index, array_keys($submenu[$menu_id]));
-			$moved_items = $submenu_items - $split_position - 1;
-
-			// clean positions from $this->menu_split_index + 1
-			$this->submenu_move($this->menu_split_index + 1, $this->menu_split_index + $moved_items + 11, $moved_items);
-		}
+		// $this->debug_print();
+		if($this->get_split_index() === null) return $menu_order;
 
 		// modify main menu items
-	    $menu_modify = $this->custom_admin_menu();
+	    $modify = $this->custom_admin_menu();
 
-		if(isset($menu_modify['reorder'])) {
-		    foreach($menu_modify['reorder'] as $menu_item) {
-		    	$this->menu_reorder($menu_item['menu'], $this->get_new_index($menu_item));
-		    }
-		}
+		if(!empty($modify)) {
+			if(isset($modify['reorder'])) {
+			    foreach($modify['reorder'] as $menu_item) {
+			    	$this->menu_reorder($menu_item['menu'], $this->get_new_index($menu_item));
+			    }
+			}
 
-    	if(isset($menu_modify['rename'])) {
-		    foreach($menu_modify['rename'] as $menu_item) {
-		    	$index = $this->get_menu_index($menu_item['menu']);
-		    	if($index > 0) $menu[$index][0] = $menu_item['new_name'];
-		    }
-		}
+	    	if(isset($modify['rename'])) {
+			    foreach($modify['rename'] as $menu_item) {
+			    	$index = $this->get_menu_index($menu_item['menu']);
+			    	if($index > 0) $menu[$index][0] = $menu_item['new_name'];
+			    }
+			}
 
-    	if(isset($menu_modify['remove'])) {
-		    foreach($menu_modify['remove'] as $menu_item) {
-		    	$index = $this->get_menu_index($menu_item['menu']);
-		    	if($index > 0) {
-			    	unset($menu[$index]);
-					ksort($menu);
-				}
-		    }
+	    	if(isset($modify['remove'])) {
+			    foreach($modify['remove'] as $menu_item) {
+			    	$index = $this->get_menu_index($menu_item['menu']);
+			    	if($index > 0) {
+				    	unset($menu[$index]);
+						ksort($menu);
+					}
+			    }
+			}
 		}
 
 		// modify submenu items
-	    $submenu_modify = $this->custom_admin_submenu();
+	    $modify = $this->custom_admin_submenu();
 
-		if(isset($submenu_modify['reorder'])) {
-		    foreach($submenu_modify['reorder'] as $menu_item) {
-		    	$submenu_parent = $menu_item['parent'] ?? $default_menu_id;
-		    	$this->submenu_reorder($menu_item['menu'], $this->get_new_index($menu_item, $submenu_parent), $submenu_parent);
-		    }
-		}
+		if(!empty($modify)) {
+			$default_id = self::$default_menu_id;
 
-    	if(isset($submenu_modify['rename'])) {
-		    foreach($submenu_modify['rename'] as $menu_item) {
-		    	$submenu_parent = $menu_item['parent'] ?? $default_menu_id;
-		    	$index = $this->get_submenu_index($menu_item['menu'], $submenu_parent);
-		    	if($index > 0) $submenu[$submenu_parent][$index][0] = $menu_item['new_name'];
-		    }
-		}
+			if(isset($modify['reorder'])) {
+			    foreach($modify['reorder'] as $menu_item) {
+			    	$parent = $menu_item['parent'] ?? $default_id;
+			    	$this->submenu_reorder($menu_item['menu'], $this->get_new_index($menu_item, $parent), $parent);
+			    }
+			}
 
-    	if(isset($submenu_modify['remove'])) {
-		    foreach($submenu_modify['remove'] as $menu_item) {
-		    	$submenu_parent = $menu_item['parent'] ?? $default_menu_id;
-		    	$index = $this->get_submenu_index($menu_item['menu'], $submenu_parent);
-		    	if($index > 0) {
-			    	unset($submenu[$submenu_parent][$index]);
-					ksort($submenu[$submenu_parent]);
-				}
-		    }
-		}
+	    	if(isset($modify['rename'])) {
+			    foreach($modify['rename'] as $menu_item) {
+			    	$parent = $menu_item['parent'] ?? $default_id;
+			    	$index = $this->get_submenu_index($menu_item['menu'], $parent);
+			    	if($index > 0) $submenu[$parent][$index][0] = $menu_item['new_name'];
+			    }
+			}
 
-		// add separators if needed
-
-		if(isset($submenu_modify['separator'])) {
-		    foreach($submenu_modify['separator'] as $menu_item) {
-		    	$submenu_parent = $menu_item['parent'] ?? $default_menu_id;
-		    	$index = $this->get_new_index($menu_item, $submenu_parent);
-		    	if($index > 0 && !isset($submenu[$submenu_parent][$index])) {
-			    	$submenu[$submenu_parent][$index] = ['','read', 'separator'.$index, '', 'wp-menu-separator'];
-					ksort($submenu[$submenu_parent]);
-				}
-		    }
+	    	if(isset($modify['remove'])) {
+			    foreach($modify['remove'] as $menu_item) {
+			    	$parent = $menu_item['parent'] ?? $default_id;
+			    	$index = $this->get_submenu_index($menu_item['menu'], $parent);
+			    	if($index > 0) {
+				    	unset($submenu[$parent][$index]);
+						ksort($submenu[$parent]);
+					}
+			    }
+			}
+			// add separators if needed
+			if(isset($modify['separator'])) {
+			    foreach($modify['separator'] as $menu_item) {
+			    	$parent = $menu_item['parent'] ?? $default_id;
+			    	$index = $this->get_new_index($menu_item, $parent);
+			    	if($index > 0 && !isset($submenu[$parent][$index])) {
+				    	$submenu[$parent][$index] = ['','read', 'separator'.$index, '', 'wp-menu-separator'];
+						ksort($submenu[$parent]);
+					}
+			    }
+			}
 		}
 
 	    return $menu_order;
@@ -169,7 +189,7 @@ trait zukit_AdminMenu {
 	protected function get_submenu_index($menu_item, $submenu_parent = null) {
 		global $submenu;
 
-		if(empty($submenu_parent)) $submenu_parent = $this->default_menu_id;
+		if(empty($submenu_parent)) $submenu_parent = self::$default_menu_id;
 
 		$index = -1;
 		// Get submenu key location based on slug
@@ -197,25 +217,32 @@ trait zukit_AdminMenu {
 	protected function submenu_move($from_index, $to_index, $count = 1, $add_separator = true, $submenu_parent = null) {
 		global $submenu;
 
-		if(empty($submenu_parent)) $submenu_parent = $this->default_menu_id;
+		if(empty($submenu_parent)) $submenu_parent = self::$default_menu_id;
 
-		while($count-- > 0) {
+		$first_was_separator = $count;
+		while($count > 0) {
 			if($to_index >= $this->total_index_break) break;
 
 			if(isset($submenu[$submenu_parent][$from_index])) {
 				$move_item = $submenu[$submenu_parent][$from_index];
+				// чтобы предотвратить ситуацию когда два сепаратора идут друг за другом
+				if($first_was_separator === $count) {
+					$first_was_separator = ($move_item[4] ?? null) === 'wp-menu-separator' ? -2 : -1;
+				}
 				unset($submenu[$submenu_parent][$from_index]);
 				while($to_index < $this->total_index_break) {
 					if(isset($submenu[$submenu_parent][$to_index])) {
 						$to_index++;
 						continue;
 					}
-					if($add_separator) {
-						$submenu[$submenu_parent][$to_index++] = ['','read', 'separator_moved', '', 'wp-menu-separator'];
+					if($add_separator && $first_was_separator !== -2) {
+						$submenu[$submenu_parent][$to_index++] = ['','read', 'separator_moved_position', '', 'wp-menu-separator'];
 						$add_separator = false;
 						continue;
 					}
+					if(self::$menu_moved_index === null) self::$menu_moved_index = $to_index;
 					$submenu[$submenu_parent][$to_index++] = $move_item;
+					$count--;
 					break;
 				}
 			}
@@ -224,5 +251,16 @@ trait zukit_AdminMenu {
 
 		// Reorder the menu based on the keys in ascending order
 	    if(isset($submenu[$submenu_parent])) ksort($submenu[$submenu_parent]);
+	}
+
+	private function debug_print($is_menu = false) {
+		global $menu, $submenu;
+
+		$items = array_map(function($item) {
+			$is_wrong = !(is_array($item) && count($item) > 2);
+			return $is_wrong ? '?' : (empty($item[0]) ? '-----'.$item[2].'-----' : $item[0]);
+		}, $is_menu ? $menu : $submenu[self::$default_menu_id]);
+		$this->log_error($items, ['called Class' => static::class]);
+		// _dbug(static::class, $items);
 	}
 }
