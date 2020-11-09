82e93ddf8d6b8ac33042c437b71c7b64bae9ffef
picasso <dmitry.rudakov@icloud.com>
2020-11-09 12:28:48 +0100

    refactoring 'skeleton' and 'sidebar' to work with new hooks

--- src/scripts/components/sidebar.js
+++ src/scripts/components/sidebar.js
@@ -11,6 +11,10 @@ import { mergeClasses, checkDependency } from './../utils.js';
 
 // Zukit Sidebar Component
 
+function availablePanels(panels, options) {
+	return pickBy(panels, p => checkDependency(p, options));
+}
+
 const ZukitSidebar = ({
 		// id,
 		version,
@@ -29,11 +33,11 @@ const ZukitSidebar = ({
 		options,
 		updateOptions,
 
-		panels,
-		updatePanels,
-		// noticeOperations = {},
+		getPanel,
+		setPanel,
 }) => {
 
+	const panels = availablePanels(getPanel(), options);
 	const moreItems = omitBy(more, isNil);
 	const pluginActions = pickBy(omitBy(actions, isNil), ({depends}) => checkDependency(depends, options, true));
 	const debugActions = omitBy(debug, isNil);
@@ -48,108 +52,107 @@ const ZukitSidebar = ({
 	}, [ajaxAction, updateOptions]);
 
 	return (
-
-			<div className="block-editor-block-inspector">
-				<div className="block-editor-block-card">
-					{ icon }
-					<div className="block-editor-block-card__content">
-						<h2 className="block-editor-block-card__title">
-							{ title }
-						</h2>
-						<span className="block-editor-block-card__description">
-						{ description }
-						</span>
-					</div>
+		<div className="block-editor-block-inspector">
+			<div className="block-editor-block-card">
+				{ icon }
+				<div className="block-editor-block-card__content">
+					<h2 className="block-editor-block-card__title">
+						{ title }
+					</h2>
+					<span className="block-editor-block-card__description">
+					{ description }
+					</span>
 				</div>
-				<PanelBody title={ __('Plugin Info') } initialOpen={ false }>
-					<PanelRow>
-						<span>{ __('Version') }</span>
-						<span>{ version }</span>
-					</PanelRow>
-					<PanelRow>
-						<span>{ __('Author') }</span>
-						<ExternalLink href={ link }>{ author }</ExternalLink>
-					</PanelRow>
-					{ hasMoreItems && map(moreItems, ({ label, value, link }, moreKey) =>
-							<PanelRow key={ moreKey }>
-								<span>{ label }</span>
-								{ link ?
-									<ExternalLink href={ link }>{ value }</ExternalLink>
-									:
-									<span>{ value }</span>
-								}
-							</PanelRow>
+			</div>
+			<PanelBody title={ __('Plugin Info') } initialOpen={ false }>
+				<PanelRow>
+					<span>{ __('Version') }</span>
+					<span>{ version }</span>
+				</PanelRow>
+				<PanelRow>
+					<span>{ __('Author') }</span>
+					<ExternalLink href={ link }>{ author }</ExternalLink>
+				</PanelRow>
+				{ hasMoreItems && map(moreItems, ({ label, value, link }, moreKey) =>
+						<PanelRow key={ moreKey }>
+							<span>{ label }</span>
+							{ link ?
+								<ExternalLink href={ link }>{ value }</ExternalLink>
+								:
+								<span>{ value }</span>
+							}
+						</PanelRow>
+				) }
+				<PanelRow>
+					<Button
+						className="__plugin_actions admin-blue"
+						icon={ 'admin-settings' }
+						isSecondary
+						isLarge
+						onClick={ resetOptions }
+					>
+						{ __('Reset Plugin Options') }
+					</Button>
+				</PanelRow>
+			</PanelBody>
+			{ hasActions &&
+				<PanelBody title={ __('Actions') } initialOpen={ true }>
+					{ map(pluginActions, ({ label, value, icon, color, help, depends }, actionKey) => (
+						checkDependency(depends, options, true) &&
+							<Fragment key={ actionKey }>
+								<PanelRow>
+									<Button
+										className={ mergeClasses('__plugin_actions', {
+											[color]: color,
+											'is-loading': get(actionLoading, value),
+											})
+										}
+										icon={ icon }
+										isSecondary
+										isLarge
+										onClick={ () => ajaxAction(value) }
+									>
+										{ label }
+										{ get(actionLoading, value) && <Spinner/> }
+									</Button>
+								</PanelRow>
+								{ help && <p className={ mergeClasses('__help', { [color]: color }) }>{ help }</p> }
+							</Fragment>
+						)
 					) }
-					<PanelRow>
-						<Button
-							className="__plugin_actions admin-blue"
-							icon={ 'admin-settings' }
-							isSecondary
-							isLarge
-							onClick={ resetOptions }
-						>
-							{ __('Reset Plugin Options') }
-						</Button>
-					</PanelRow>
 				</PanelBody>
-				{ hasActions &&
-					<PanelBody title={ __('Actions') } initialOpen={ true }>
-						{ map(pluginActions, ({ label, value, icon, color, help, depends }, actionKey) => (
-							checkDependency(depends, options, true) &&
-								<Fragment key={ actionKey }>
-									<PanelRow>
-										<Button
-											className={ mergeClasses('__plugin_actions', {
-												[color]: color,
-												'is-loading': get(actionLoading, value),
-												})
-											}
-											icon={ icon }
-											isSecondary
-											isLarge
-											onClick={ () => ajaxAction(value) }
-										>
-											{ label }
-											{ get(actionLoading, value) && <Spinner/> }
-										</Button>
-									</PanelRow>
-									{ help && <p className={ mergeClasses('__help', { [color]: color }) }>{ help }</p> }
-								</Fragment>
-							)
-						) }
-					</PanelBody>
-				}
-				{ hasPanels &&
-					<PanelBody title={ __('Screen Options') } initialOpen={ false }>
-						{ map(panels, ({ label, value, help }, panelKey) =>
-							<ToggleControl
-								key={ panelKey }
-								label={ label }
-								help={ help  }
-								checked={ value }
-								onChange={ () => updatePanels({ [panelKey]: !value }) }
-							/>
-						) }
-					</PanelBody>
-				}
-				{ hasDebugActions &&
-					<PanelBody title={ __('Debug Actions') } initialOpen={ false }>
-						{ map(debugActions, ({ label, value, icon, color }, actionKey) =>
-							<PanelRow key={ actionKey }>
-								<Button
-									className={ mergeClasses('__plugin_actions', { [color]: color }) }
-									icon={ icon }
-									isSecondary
-									isLarge
-									onClick={ () => ajaxAction(value) }
-								>
-									{ label }
-								</Button>
-							</PanelRow>
-						) }
-					</PanelBody>
-				}
-				</div>
+			}
+			{ hasPanels &&
+				<PanelBody title={ __('Screen Options') } initialOpen={ false }>
+					{ map(panels, ({ label, value, help }, panelKey) =>
+						<ToggleControl
+							key={ panelKey }
+							label={ label }
+							help={ help  }
+							checked={ value }
+							onChange={ () => setPanel({ [panelKey]: !value }) }
+						/>
+					) }
+				</PanelBody>
+			}
+			{ hasDebugActions &&
+				<PanelBody title={ __('Debug Actions') } initialOpen={ false }>
+					{ map(debugActions, ({ label, value, icon, color }, actionKey) =>
+						<PanelRow key={ actionKey }>
+							<Button
+								className={ mergeClasses('__plugin_actions', { [color]: color }) }
+								icon={ icon }
+								isSecondary
+								isLarge
+								onClick={ () => ajaxAction(value) }
+							>
+								{ label }
+							</Button>
+						</PanelRow>
+					) }
+				</PanelBody>
+			}
+		</div>
 	);
 }
 
--- src/scripts/components/skeleton.js
+++ src/scripts/components/skeleton.js
@@ -1,40 +1,24 @@
 // WordPress dependencies
 
-const {
-	keys,
-	has,
-	get,
-	set,
-	includes,
-	forEach,
-	pickBy,
-	omit,
-	mapValues,
-	reduce,
-	first,
-	castArray,
-} = lodash;
+const { keys, has, get, includes, forEach, omit, reduce, first, castArray } = lodash;
 const { __ } = wp.i18n;
 const { compose } = wp.compose;
 const { RawHTML } = wp.element;
 const { BlockIcon } = wp.blockEditor;
 const { withNotices } = wp.components;
-const { useState, useCallback, useRef, useEffect } = wp.element;
+const { useState, useCallback, useEffect } = wp.element;
 
 // Internal dependencies
 
-import { ajaxUpdateOptions, ajaxDoAction } from './../fetch.js';
-import { checkDependency } from './../utils.js';
+import { ajaxDoAction } from './../fetch.js';
+import { useOptions } from './../hooks/use-options.js';
+import { usePanels } from './../hooks/use-panels.js';
+import NoticesContext from './../hooks/use-notices.js';
 import ZukitSidebar from './sidebar.js';
-import { withZukitPanel } from './panel.js';
 
 // Zukit Skeleton Component
 
 const cprefix = 'zukit-skeleton';
-
-function availablePanels(panels, options) {
-	return pickBy(panels, p => checkDependency(p, options));
-}
 
 const ZukitSkeleton = ({
 		id,
@@ -49,9 +33,6 @@ const ZukitSkeleton = ({
 }) => {
 
 	const { createNotice } = noticeOperations;
-	const hooks = useRef(null);
-	const [options, setOptions] = useState(initialOptions);
-	const [panels, setPanels] = useState(initialPanels);
 	const [moreInfo, setMoreInfo] = useState(info.more || {});
 	const [loading, setLoading] = useState(
 		// create object {action_value: state}
@@ -66,39 +47,14 @@ const ZukitSkeleton = ({
 		}
 	}, [loading]);
 
-	const updateOptions = useCallback((update, reset = false) => {
-		// если 'reset' is true - вызываем c null вместо 'keys' что приведет к проверке всех 'hooks'
-		if(reset) {
-			ajaxUpdateOptions(null, { prev: options, next: update }, null, hooks.current);
-		} else {
-			ajaxUpdateOptions(keys(update), update, createNotice, hooks.current);
-			// используем функцию 'set' вместо простого присвоения, потому что 'key'
-			// может представлять из себя 'path'
-			forEach(update, (value, key) => set(options, key, value));
-		}
-		// если 'reset' is true - просто обновляем state значением из 'update'
-		setOptions({ ...(reset ? update : options) });
-	}, [options, createNotice]);
+	// create 'options' and setter
+	const [options, updateOptions, setUpdateHook] = useOptions(initialOptions, createNotice);
+	// create getter and setter for 'panels'
+	const [getPanel, setPanel, PanelsContext] = usePanels(initialPanels, createNotice);
 
 	const ajaxAction = useCallback((params, callback) => {
 		ajaxDoAction(params, callback, createNotice, updateLoading);
 	}, [createNotice, updateLoading]);
-
-	const updatePanels = useCallback(update => {
-		// используем функцию 'set' чтобы поменять только 'value', а не весь объект
-		forEach(update, (value, key) => set(panels, `${key}.value`, value));
-		// не используем функцию 'setOptions' чтобы не вызывать не нужное обновление 'options'
-		// чтоб приведет к излишним renders - нам нужно только сохранить 'panels' в базе
-		ajaxUpdateOptions('panels', mapValues(panels, p => p.value), createNotice);
-		setPanels({ ...panels });
-	}, [panels, createNotice]);
-
-	const setUpdateHook = useCallback((key, callback) => {
-		var keys = castArray(key);
-		// create object for all keys {key: callback}
-		var callbacks = reduce(keys, (acc, val) => (acc[val] = callback, acc), {});
-		hooks.current = { ...(hooks.current || {}), ...callbacks };
-	}, []);
 
 	// set hooks to update info.more items when dependency keys (in 'depends') are updated in 'options'
 	useEffect(() => {
@@ -114,19 +70,21 @@ const ZukitSkeleton = ({
 		});
 	}, [moreInfo, setUpdateHook, ajaxAction]);
 
-	const pluginEdit = !EditComponent ? null : (
-		<EditComponent
-			id={ id }
-			info={ info }
-			title={ `${info.title} ${__('Settings')}` }
-			// panels={ panels }
-			options={ options }
-			zukitPanel={ withZukitPanel(panels) }
-			updateOptions={ updateOptions }
-			ajaxAction={ ajaxAction }
-			noticeOperations={ noticeOperations }
-			setUpdateHook={ setUpdateHook }
-		/>
+	const pluginOptionsEdit = !EditComponent ? null : (
+		<NoticesContext.Provider value={ noticeOperations }>
+			<PanelsContext.Provider value={ getPanel }>
+				<EditComponent
+					id={ id }
+					info={ info }
+					title={ `${info.title} ${__('Settings')}` }
+					options={ options }
+					updateOptions={ updateOptions }
+					ajaxAction={ ajaxAction }
+					noticeOperations={ noticeOperations }
+					setUpdateHook={ setUpdateHook }
+				/>
+			</PanelsContext.Provider>
+		</NoticesContext.Provider>
 	);
 
 	const pluginIcon = !info.icon ? null : (
@@ -137,65 +95,61 @@ const ZukitSkeleton = ({
 	);
 
 	return (
-		<>
-			<div className={ `${cprefix} edit-post-layout is-mode-visual is-sidebar-opened block-editor-editor-skeleton` }>
-				<div className="block-editor-editor-skeleton__body">
-					<div
-						className="block-editor-editor-skeleton__content"
-						role="region"
-						aria-label="Settings content"
-						tabIndex="-1"
-					>
-						<div className="components-editor-notices__dismissible">
-							{ noticeUI }
-						</div>
-						<div className="edit-post-visual-editor editor-styles-wrapper" tabIndex="-1">
-							<div className="block-editor-block-list__layout">
-								<div className="wp-block block-editor-block-list__block">
-									<div className="editor-post-title">
-										<div className="wp-block editor-post-title__block">
-											<h1>{ info.title }</h1>
-											{ pluginIcon }
-										</div>
-									</div>
-									{ pluginEdit }
-								</div>
-							</div>
-						</div>
+		<div className={ `${cprefix} edit-post-layout is-mode-visual is-sidebar-opened block-editor-editor-skeleton` }>
+			<div className="block-editor-editor-skeleton__body">
+				<div
+					className="block-editor-editor-skeleton__content"
+					role="region"
+					aria-label="Settings content"
+					tabIndex="-1"
+				>
+					<div className="components-editor-notices__dismissible">
+						{ noticeUI }
 					</div>
-					<div
-						className="block-editor-editor-skeleton__sidebar"
-						role="region"
-						aria-label="Plugin settings"
-						tabIndex="-1"
-					>
-						<div>
-							<div className="edit-post-sidebar">
-								<ZukitSidebar
-									id={ id }
-									icon={ pluginIcon }
-									more={ moreInfo }
-									actions={ actions }
-									actionLoading={ loading }
-									debug={ debug }
-									ajaxAction={ ajaxAction }
-
-									options={ options }
-									updateOptions={ updateOptions }
-
-									panels={ availablePanels(panels, options) }
-									updatePanels={ updatePanels }
-
-									noticeOperations={ noticeOperations }
-									{ ...omit(info, ['icon', 'more']) }
-								/>
+					<div className="edit-post-visual-editor editor-styles-wrapper" tabIndex="-1">
+						<div className="block-editor-block-list__layout">
+							<div className="wp-block block-editor-block-list__block">
+								<div className="editor-post-title">
+									<div className="wp-block editor-post-title__block">
+										<h1>{ info.title }</h1>
+										{ pluginIcon }
+									</div>
+								</div>
+								{ pluginOptionsEdit }
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+				<div
+					className="block-editor-editor-skeleton__sidebar"
+					role="region"
+					aria-label="Plugin settings"
+					tabIndex="-1"
+				>
+					<div>
+						<div className="edit-post-sidebar">
+							<ZukitSidebar
+								id={ id }
+								icon={ pluginIcon }
+								more={ moreInfo }
+								actions={ actions }
+								actionLoading={ loading }
+								debug={ debug }
+								ajaxAction={ ajaxAction }
 
-		</>
+								options={ options }
+								updateOptions={ updateOptions }
+
+								getPanel={ getPanel }
+								setPanel={ setPanel }
+
+								{ ...omit(info, ['icon', 'more']) }
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
