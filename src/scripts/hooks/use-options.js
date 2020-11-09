6ef75dfcabe17ae24f0179ad5b80f392fa29e2b9
picasso <dmitry.rudakov@icloud.com>
2020-11-09 12:27:41 +0100

    implemented work with 'options' and 'panels' via reducer to decrease re-renders

---
+++ src/scripts/hooks/use-options.js
@@ -0,0 +1,64 @@
+// WordPress dependencies
+
+const { forEach, set, isPlainObject, isFunction, keys, castArray, reduce } = lodash;
+const { useCallback, useReducer, useRef } = wp.element;
+
+// Internal dependencies
+
+import { ajaxUpdateOptions } from './../fetch.js';
+
+// The state update logic with the useReducer() -------------------------------]
+
+function optionsReducer(options, action) {
+	switch(action.type) {
+		case 'set':
+			// payload -> update object { key: value, key2: value2 }
+			// используем функцию 'set' вместо простого присвоения, потому что 'key'
+			// может представлять из себя 'path'
+			forEach(action.payload, (value, key) => set(options, key, value));
+			// we can't just return 'options' with changes, we must return a new object
+			return { ...options };
+		case 'pre-reset':
+			// payload -> callback function
+			if(isFunction(action.payload)) action.payload(options);
+			return options;
+		case 'reset':
+			// payload -> new options object
+			return action.payload;
+		default:
+			return options;
+	}
+ }
+
+// Update 'options' atributte
+// Create callback to pass down a 'dispatch' function from 'useReducer' via context
+export function useOptions(initialOptions, createNotice) {
+
+	const [options, dispatch] = useReducer(optionsReducer, initialOptions);
+	const hooks = useRef(null);
+
+	const updateOptions = useCallback((update, reset = false) => {
+
+		if(!isPlainObject(update)) return;
+
+		if(reset) {
+			// forced to use the callback trick to get the current 'options'
+			dispatch({ type: 'pre-reset', payload: prevOptions => {
+				ajaxUpdateOptions(null, { prev: prevOptions, next: update }, null, hooks.current);
+				dispatch({ type: 'reset', payload: update });
+			} });
+		} else {
+			ajaxUpdateOptions(keys(update), update, createNotice, hooks.current);
+			dispatch({ type: 'set', payload: update });
+		}
+	}, [createNotice]);
+
+	const setUpdateHook = useCallback((key, callback) => {
+		var keys = castArray(key);
+		// create object for all keys {key: callback}
+		var callbacks = reduce(keys, (acc, val) => (acc[val] = callback, acc), {});
+		hooks.current = { ...(hooks.current || {}), ...callbacks };
+	}, []);
+
+	return [options, updateOptions, setUpdateHook];
+}
