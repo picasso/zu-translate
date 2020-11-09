8383d8d44a2f936f8d29318fd905815234f5ed60
picasso <dmitry.rudakov@icloud.com>
2020-11-09 12:35:10 +0100

    small improvements

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -1,14 +1,24 @@
 // Components
-import ConditionalWrap from './conditional-wrap.js';
 import ZukitDivider from './divider.js';
 import ZukitPanel from './panel.js';
-import SelectItem from './select-item.js';
 import ZukitSidebar from './sidebar.js';
 import ZukitSkeleton from './skeleton.js';
 import ZukitTable from './table.js';
 
-// Higher-Order Components
-// export { default as navigateRegions } from './higher-order/navigate-regions';
-// export { default as withConstrainedTabbing } from './higher-order/with-constrained-tabbing';
+import ConditionalWrap from './conditional-wrap.js';
+import SelectItem from './select-item.js';
+import ListInput from './list-input.js';
 
-export { ConditionalWrap, ZukitDivider, ZukitPanel, SelectItem, ZukitSidebar, ZukitSkeleton, ZukitTable };
+// ??? Higher-Order Components
+
+export {
+    ZukitSkeleton,
+    ZukitSidebar,
+    ZukitPanel,
+    ZukitDivider,
+    ZukitTable,
+
+    ConditionalWrap,
+    SelectItem,
+    ListInput,
+};
