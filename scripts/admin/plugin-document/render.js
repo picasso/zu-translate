cc0576f6af55f43ddbafd4527c3eff8d934e5069
picasso <dmitry.rudakov@icloud.com>
2021-12-28 11:31:42 +0100

    added 'title indicator' to document plugin

--- scripts/admin/plugin-document/render.js
+++ scripts/admin/plugin-document/render.js
@@ -1,56 +1,28 @@
 // WordPress dependencies
 
-const { reduce, has } = lodash;
-const { compose } = wp.compose;
-const { withSelect, withDispatch } = wp.data;
+// const { reduce, has } = lodash;
+// const { compose } = wp.compose;
+// const { withSelect, withDispatch } = wp.data;
 const { useCallback, useEffect } = wp.element;
 
 // Zukit dependencies
 
-// const { SelectItemControl } = wp.zukit.components;
-// const { toRange, isNum } = wp.zukit.utils;
+const { withPlugin } = wp.zukit.plugins;
+const { useForceUpdater } = wp.zukit.data;
 
 // Internal dependencies
 
 import metadata from './metadata.js';
-import withSidebar from './../components/with-sidebar.js';
 import LangControl from './../components/lang-control.js';
-import { changeLang, setRawAttributes, switchRawAttributes, useForceUpdater, useOnLangChange } from './../data/use-store.js';
+import { changeLang, getLang, setRawAttributes, switchRawAttributes, useOnLangChange } from './../data/use-store.js';
 
-// LangControlSetting Edit Component
+// LangControlSetting Component
 
-// innerBlocks
-
-function collectAllRelatedBlocks(blocks) {
-	return reduce(blocks, (collected, {clientId, innerBlocks, attributes}) => {
-		if(has(attributes, 'qtxLang')) collected.push(clientId);
-		if(innerBlocks.length) {
-			const innerIds = collectAllRelatedBlocks(innerBlocks);
-			collected.push(...innerIds);
-		}
-		return collected;
-	}, []);
-}
-
-export function switchAllRelatedBlocks(lang, getBlocks, updateBlockAttributes, clientId = null) {
-	const allBlocks = getBlocks();
-	const blockIds = collectAllRelatedBlocks(allBlocks);
-	Zubug.data({ allBlocks, blockIds });
-
-	blockIds.forEach(blockClientId => {
-		// do not update own attributes
-		if(blockClientId !== clientId) {
-			updateBlockAttributes(blockClientId, { qtxLang: lang } );
-		}
-	});
-}
-
-const LangControlSetting = (
-// {
-// 	getBlocks,
-// 	updateBlockAttributes,
-// }
-) => {
+const LangControlSetting = ({
+	forceUpdate: forceUpdateParent,
+	// getBlocks,
+	// updateBlockAttributes,
+}) => {
 
 	const forceUpdate = useForceUpdater();
 	const editorLang = useOnLangChange(switchRawAttributes); //'() => {}); // switchRawAttributes
@@ -66,8 +38,9 @@ const LangControlSetting = (
 		// updateRawAttributes();
 		changeLang(value);
 		forceUpdate();
+		forceUpdateParent();
 		// switchAllRelatedBlocks(value, getBlocks, updateBlockAttributes);
-	}, [forceUpdate]);
+	}, [forceUpdate, forceUpdateParent]);
 
 	return (
 		<LangControl
@@ -77,13 +50,47 @@ const LangControlSetting = (
 	);
 }
 
-export default compose([
-	withSidebar(metadata),
-	withSelect(select => {
-		return { getBlocks: select('core/block-editor').getBlocks };
-	}),
-	withDispatch(dispatch => {
-		// const { updateBlockAttributes } = dispatch('core/block-editor');
-		return { updateBlockAttributes: dispatch('core/block-editor').updateBlockAttributes };
-	}),
-])(LangControlSetting);
+const pluginProps = {
+	...metadata,
+	title: LangControl.Indicator,
+	titleProps: () => ({ lang: getLang() }),
+}
+
+export default withPlugin(pluginProps)(LangControlSetting);
+
+// NOTE: All experiments left here --------------------------------------------]
+
+// function collectAllRelatedBlocks(blocks) {
+// 	return reduce(blocks, (collected, {clientId, innerBlocks, attributes}) => {
+// 		if(has(attributes, 'qtxLang')) collected.push(clientId);
+// 		if(innerBlocks.length) {
+// 			const innerIds = collectAllRelatedBlocks(innerBlocks);
+// 			collected.push(...innerIds);
+// 		}
+// 		return collected;
+// 	}, []);
+// }
+//
+// function switchAllRelatedBlocks(lang, getBlocks, updateBlockAttributes, clientId = null) {
+// 	const allBlocks = getBlocks();
+// 	const blockIds = collectAllRelatedBlocks(allBlocks);
+// 	Zubug.data({ allBlocks, blockIds });
+//
+// 	blockIds.forEach(blockClientId => {
+// 		// do not update own attributes
+// 		if(blockClientId !== clientId) {
+// 			updateBlockAttributes(blockClientId, { qtxLang: lang } );
+// 		}
+// 	});
+// }
+
+// export default compose([
+// 	withPluginNoMeta(metadata),
+// 	withSelect(select => {
+// 		return { getBlocks: select('core/block-editor').getBlocks };
+// 	}),
+// 	withDispatch(dispatch => {
+// 		// const { updateBlockAttributes } = dispatch('core/block-editor');
+// 		return { updateBlockAttributes: dispatch('core/block-editor').updateBlockAttributes };
+// 	}),
+// ])(LangControlSetting);
