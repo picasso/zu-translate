2693d9bb4542281fc7f43e84a86731a663554d8e
picasso <dmitry.rudakov@icloud.com>
2022-01-03 18:47:13 +0100

    refactoring RAW helpers

--- scripts/admin/utils.js
+++ scripts/admin/utils.js
@@ -52,10 +52,6 @@ export function hasTranslations(text) {
 	const blocks = qtranxj_get_split_blocks(text ?? '');
 	// no language separator found - there are no translations
 	return blocks?.length > 1;
-
-	// if (!blocks || !blocks.length)
-	// 	return result;
-	// if (blocks.length === 1) {
 }
 
 export function getLangContent(raw, lang) {
@@ -64,56 +60,74 @@ export function getLangContent(raw, lang) {
 }
 
 export function createRawContent(lang, values, translatedAtts, maybeFixRaw = false) {
-    if(values) {
-        const separator = marker(testDelimters(), null, null, true);
-        const rawItems = _.fill(_.range(0, values.length), '');
-		// if RAW was created for wrong amount of attributes
-		if(maybeFixRaw) {
-			const items = _.split(maybeFixRaw, separator);
-			if(values.length !== items.length) {
-				maybeFixRaw = fixRawContent(lang, values, separator, maybeFixRaw);
-			}
-			return [ maybeFixRaw, {} ];
+	const del = getDefaultDelimter();
+    const separator = getSeparator();
+    const rawItems = emptyRawContent(values.length, false);
+// _.fill(_.range(0, values.length), '');
+	// if RAW was created for wrong amount of attributes
+	if(maybeFixRaw) {
+		const items = _.split(maybeFixRaw, separator);
+		if(values.length !== items.length) {
+			maybeFixRaw = fixRawContent(lang, values, separator, maybeFixRaw);
 		}
-        const prevRaw = _.reduce(values, (foundRaw, value) => {
-            return foundRaw === false ? (hasTranslations(value) ? value : false) : foundRaw;
-        }, false);
-        if(activateDebug) Zubug.data({ lang, values, prevRaw });
-        if(prevRaw) {
-            const index = _.indexOf(values, prevRaw);
-            const atts = _.split(translatedAtts ?? '', ',');
-            _.set(rawItems, index, prevRaw);
-            return [
-                _.join(rawItems, separator),
-                { [atts[index]] : getLangContent(prevRaw, lang) },
-            ];
-        }
-        const raw = _.join(rawItems, separator);
-        return [ updateRawContent(raw, lang, values), {} ];
+		return [ maybeFixRaw, {} ];
+	}
+	// check if any of the attributes already contains a RAW block
+    const prevRaw = _.reduce(values, (foundRaw, value) => {
+        return foundRaw === false ? (hasTranslations(value) ? value : false) : foundRaw;
+    }, false);
+    if(activateDebug) Zubug.data({ lang, values, prevRaw });
+
+	// if we already had RAW in some attribute, we reformat the RAW with this in mind
+	// NOTE: the option when more than one attribute contained RAW is not processed
+	// this seems to be impossible - before this plugin the RAW was not split by attributes
+	// return the new RAW and attribute content for the current language
+    if(prevRaw) {
+        const index = _.indexOf(values, prevRaw);
+        const atts = _.split(translatedAtts ?? '', ',');
+		const newItems = updateRawContent(rawItems, lang, values, { del: testDelimters(prevRaw), separator, join: false })
+        _.set(newItems, index, prevRaw);
+        return [
+            _.join(newItems, separator),
+            { [atts[index]] : getLangContent(prevRaw, lang) },
+        ];
     }
-    return [ null, {} ];
+
+	// for the newly created RAW, save the current attribute values in the current language section
+// const raw = _.join(rawItems, separator);
+    return [ updateRawContent(rawItems, lang, values, { del, separator }), {} ];
 }
 
-export function updateRawContent(raw, lang, values = null) {
-    if(activateDebug) Zubug.data({ raw, values });
-    if(values) {
-        const del = testDelimters(raw);
-        const separator = marker(del, null, null, true);
-        const content = _.split(raw, separator);
+export function updateRawContent(raw, lang, values, options = null) {
+    if(activateDebug) Zubug.data({ raw, lang, values });
+	if(lang === undefined) return raw;
+    const del = _.get(options, 'del') ?? testDelimters(raw);
+    const separator = _.get(options, 'separator') ?? getSeparator(del);
+	const joinItems =  _.get(options, 'join', true);
+    const items = _.isArray(raw) ? raw : _.split(raw, separator);
 
-        const rawItems = _.reduce(content, (newRaw, rawItem, index) => {
-            const blocks = getTranslatedBlocks(rawItem);
-            if(values[index] !== undefined) {
-                blocks[lang] = values[index];
-                const withMarkers = _.map(blocks, (text, ln) => marker(del, ln, text));
-                newRaw[index] = _.join([...withMarkers, marker(del)], '');
-            }
-            return newRaw;
-        }, []);
-        if(activateDebug) Zubug.data({ del, separator, content, rawItems });
-        return _.join(rawItems, separator);
-    }
-    return raw;
+    const rawItems = _.reduce(items, (newRaw, rawItem, index) => {
+        const blocks = getTranslatedBlocks(rawItem);
+        if(values[index] !== undefined) {
+            blocks[lang] = values[index];
+            const withMarkers = _.map(blocks, (text, ln) => marker(del, ln, text));
+            newRaw[index] = _.join([...withMarkers, marker(del)], '');
+        }
+        return newRaw;
+    }, []);
+    if(activateDebug) Zubug.data({ del, separator, items, rawItems });
+    return joinItems ? _.join(rawItems, separator) : rawItems;
+}
+
+// fix if RAW was created for wrong amount of attributes
+export function maybeFixRawContent(prevRaw, lang, values) {
+	const separator = getSeparator();
+	const items = _.split(prevRaw, separator);
+	if(values.length !== items.length) {
+		const fixedRaw = fixRawContent(lang, values, separator, prevRaw);
+		return prevRaw !== fixedRaw ? fixedRaw : false;
+	}
+	return false;
 }
 
 export function switchContent(raw, lang, translatedAtts) {
@@ -126,6 +140,8 @@ export function switchContent(raw, lang, translatedAtts) {
         return attributes;
     }, {});
 }
+
+// DOM manipulations ----------------------------------------------------------]
 
 export function getInputValue(selector) {
 	return document.querySelector(selector)?.value ?? null;
@@ -146,14 +162,35 @@ export function addInputListener(selector, callback) {
 	const el = document.querySelector(selector);
 	// React chose to make 'onChange' behave like 'onInput' does
 	// it does fire when there's a change, just not until the input also loses focus
+	// that's why we are adding a listener to the 'input' event and not to the 'change' event
     if(el) el.addEventListener('input', callback);
 }
 
 // internal helpers -----------------------------------------------------------]
 
+function emptyRawContent(itemCount, joinItems = true) {
+	const del = getDefaultDelimter();
+	const separator = getSeparator(del);
+	const blocks = getTranslatedBlocks('');
+	const withMarkers = _.map(blocks, (text, ln) => marker(del, ln, text));
+	const emptyRaw = _.join([...withMarkers, marker(del)], '');
+	const rawItems = _.fill(_.range(0, itemCount), emptyRaw);
+    return joinItems ? _.join(rawItems, separator) : rawItems;
+}
+
+function splitRawContent(raw) {
+    const del = testDelimters(raw);
+    const separator = getSeparator(del);
+    return _.split(raw, separator);
+}
+
+function getTranslatedBlocks(raw) {
+	return qtranxj_split(raw ?? '');
+}
+
 // if RAW was created for wrong amount of attributes
 function fixRawContent(lang, values, separator, currentRaw) {
-	let raw = emptyRawContent(values.length, separator);
+	let raw = emptyRawContent(values.length);
 	_.forEach(values, (val, index) => {
 		if(val !== '' && val !== undefined) {
 			if(_.includes(currentRaw, val)) {
@@ -161,31 +198,21 @@ function fixRawContent(lang, values, separator, currentRaw) {
 				rawItems[index] = currentRaw;
 				raw = _.join(rawItems, separator);
 			} else {
-				raw = updateRawContent(raw, lang, values);
+				raw = updateRawContent(raw, lang, values, { separator });
 			}
 		}
 	});
 	return raw;
 }
 
-function emptyRawContent(items) {
-	const del = testDelimters('');
-	const separator = marker(del, null, null, true);
-	const blocks = getTranslatedBlocks('');
-	const withMarkers = _.map(blocks, (text, ln) => marker(del, ln, text));
-	const emptyRaw = _.join([...withMarkers, marker(del)], '');
-	const rawItems = _.fill(_.range(0, items), emptyRaw);
-    return _.join(rawItems, separator);
+// 'delimter' is what separates blocks of text in different languages
+function getDefaultDelimter() {
+	return testDelimters();
 }
 
-function splitRawContent(raw) {
-    const del = testDelimters(raw);
-    const separator = marker(del, null, null, true);
-    return _.split(raw, separator);
-}
-
-function getTranslatedBlocks(raw) {
-	return qtranxj_split(raw ?? '');
+// 'separator' is what separates RAW blocks for different attributes
+function getSeparator(del) {
+	return marker(del ?? getDefaultDelimter(), null, null, true);
 }
 
 function splitInHalf(s) {
