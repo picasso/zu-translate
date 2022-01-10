9f2eb8001ecc299b701855f0cb31283d2055eb8d
picasso <dmitry.rudakov@icloud.com>
2022-01-10 16:03:43 +0100

    refactoring 'supported_data' and 'supported_blocks' usage

--- includes/traits/supported.php
+++ includes/traits/supported.php
@@ -4,7 +4,13 @@
 
 trait zu_TranslateSupportedBlocks {
 
+	// 'supported_data' - is a list of all registered blocks, those by default and added by the user
+	private $supported_data = null;
+	// 'supported_blocks' - is a list of all registered blocks minus blocks from 'excluded' list
+	private $supported_blocks = null;
+
 	private $wp_synced = '5.8.2';
+	private $external_blocks = [];
 
 	private $wp_text_blocks = [
 		'core/paragraph'		=> ['name' => 'Paragraph', 'atts' => 'content'],
@@ -44,15 +50,43 @@ trait zu_TranslateSupportedBlocks {
 		// as far as i understand, there is no content here, which requires translation
 	];
 
+	public function register_translated_blocks($blocks) {
+		$result = 0;
+		if(is_array($blocks)) {
+			foreach($blocks as $name => $block) {
+				if(array_key_exists($name, $this->external_blocks)) continue;
+				if(is_array($block) && array_key_exists('atts', $block)) {
+					$this->external_blocks[$name] = $block;
+					$result += 1;
+				}
+			}
+		}
+		if($result > 0) $this->assign_supported_blocks();
+		return $result;
+	}
 
-	private function get_wp_supported_blocks() {
+	private function assign_supported_blocks() {
+		$supported = $this->get_option('blockeditor.custom', []);
+		$supported = array_merge(is_array($supported) ? $supported : [], $this->get_all_supported_blocks());
+		$this->supported_data = $supported;
+		$excluded = $this->get_option('blockeditor.excluded', []);
+		$this->supported_blocks = $this->snippets('array_without_keys', $supported, $excluded);
+	}
+
+	private function get_all_supported_blocks() {
 		return array_merge(
 			$this->wp_text_blocks,
 			$this->wp_media_blocks,
 			$this->wp_design_blocks,
 			$this->wp_widgets_blocks,
 			$this->wp_theme_blocks,
-			$this->wp_embeds_blocks
+			$this->wp_embeds_blocks,
+
+			$this->external_blocks
 		);
+	}
+
+	private function get_registered_data($kind = 'blocks') {
+		return ($kind === 'blocks' ? $this->supported_blocks : $this->supported_data) ?? [];
 	}
 }
