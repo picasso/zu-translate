1cb4da9f253aeac5fa1a98111ecd647f71b8a360
picasso <dmitry.rudakov@icloud.com>
2022-01-20 19:42:15 +0100

    keep some ideas

--- scripts/admin/__ideas.js
+++ scripts/admin/__ideas.js
@@ -31,6 +31,59 @@ function sendSuccessResponse(responseData) {
 	);
 }
 
+// forEach(supportedAtts, (attr, selector) => {
+// 	if(addListeners) {
+// 		let value = getRaw(attr);
+// 		if(value === undefined) {
+// 			value = getEditedPostAttribute(`${attr}_raw`);
+// 			if(attr === 'title' && value === 'Auto Draft') value = '';
+// 			setRaw(attr, value);
+// 			addInputListener(selector, getListener(attr));
+//
+// 			// 'title' does not require 'inserted hook' as it is not removed from the page
+// 			// when the attribute Panel is closed
+// 			if(attr !== 'title') attachInsertedHooks(attr, selector);
+// 		} else {
+// 			addInputListener(selector, getListener(attr));
+// 			// if the language has been switched while editing blocks
+// 			// then synchronize switching for newly created elements (for example, 'excerpt')
+// 			// 'title' does not require synchronization as it is not removed from the page while editing blocks
+// 			if(attr !== 'title') switchRawAttributes(null, attr);
+// 			debug.info(`set for {${attr}} is ignored, current value = ${value}`);
+// 		}
+// 	} else {
+// 		// with the third argument equal to false listener will be removed
+// 		addInputListener(selector, getListener(attr), false);
+// 	}
+// });
+
+// update RAW attributes before changing language
+// (if 'onlyAtts' is not null - update RAW for these attributes only)
+// export function updateRawAttributes(onlyAtts = null) {
+// 	onlyAtts = onlyAtts === null ? null : castArray(onlyAtts);
+// 	forEach(supportedAtts, (selector, attr) => {
+// 		if(onlyAtts === null || includes(onlyAtts, attr)) {
+// 			const value = getInputValue(selector);
+// 			updateRaw(attr, value);
+// 		}
+// 	});
+// }
+
+// listeners on changes and on DOM 'insert' -----------------------------------]
+
+// we need to pre-create listeners for each attribute,
+// since we cannot use arrow function directly when adding listener - later we won't be able to remove such listener
+
+// const listeners = {};
+
+// forEach(supportedKeys, attr => {
+// 	listeners[attr] = () => updateRawAttributes(attr);
+// });
+
+// function getListener(attr) {
+// 	return listeners[attr] ?? noop;
+// }
+
 
 // const sidebarRoot = '.edit-post-sidebar > .components-panel';
 //
