84e167d4130afeeee69708be31cf8ef3cc21648d
picasso <dmitry.rudakov@icloud.com>
2022-02-23 19:18:41 +0100

    refactoring blocks conversion;added 'split' conversion

--- includes/traits/converted.php
+++ includes/traits/converted.php
@@ -4,12 +4,86 @@
 
 trait zu_TranslateConverted {
 
-	private function convert_blocks($post_id, $post_lang = 'en') {
+	private function convert_classic_blocks($post_id, $post_lang = 'en') {
 		$post = get_post($post_id);
 		$content = $post->post_content;
-// zu_log($content);
+		$found_blocks = $this->find_blocks($content);
+		if(empty($found_blocks)) return false;
+
+		[ $block_tokens, $languages ] = $this->get_tokens($found_blocks);
+// zu_log($block_tokens, $languages);
+		if(!empty($languages)) {
+			foreach($found_blocks as $id => $raw) {
+				$lang_blocks = qtranxf_split($raw);
+				$tokens = $block_tokens[$id];
+
+				$headings = [];
+				$paragraphs = [];
+				foreach($languages as $lang) {
+					foreach($tokens[$lang]['headings'][1] as $index => $value) {
+						$headings[$index][$lang] = $value;
+					}
+					foreach($tokens[$lang]['text'] as $index => $value) {
+						$paragraphs[$index][$lang] = $value;
+					}
+				}
+				$block_content = $this->replace_blocks_with_raw($lang_blocks[$post_lang], [
+					'headings'			=> $tokens[$post_lang]['headings'][0],
+					'paragraphs'		=> $tokens[$post_lang]['text'],
+					'shortcodes'		=> $tokens[$post_lang]['shortcodes'],
+					'headings_raw'		=> array_map('qtranxf_join_b', $headings),
+					'paragraphs_raw'	=> array_map('qtranxf_join_b', $paragraphs),
+				]);
+// zu_log($id, $block_content, $content);
+				$content = str_replace($id, $block_content, $content);
+			}
+// zu_log($content, $tokens);
+			$this->duplicate_post_as_draft($post, $content);
+			return true;
+		}
+		return false;
+	}
+
+	private function split_classic_blocks($post_id) {
+		$post = get_post($post_id);
+		$content = $post->post_content;
+		$found_blocks = $this->find_blocks($content);
+		if(empty($found_blocks)) return false;
+
+		[ $block_tokens, $languages ] = $this->get_tokens($found_blocks);
+
+		$split_content = [];
+		if(!empty($languages)) {
+			$split_content = array_fill_keys($languages, $content);
+
+			foreach($found_blocks as $id => $raw) {
+				$lang_blocks = qtranxf_split($raw);
+				$tokens = $block_tokens[$id];
+				foreach($lang_blocks as $lang => $lang_raw) {
+					$block_content = $this->replace_blocks_with_raw($lang_raw, [
+						'without_raw'		=> true,
+						'headings'			=> $tokens[$lang]['headings'][0] ?? [],
+						'paragraphs'		=> $tokens[$lang]['text'] ?? [],
+						'shortcodes'		=> $tokens[$lang]['shortcodes'] ?? [],
+					]);
+					$split_content[$lang] = str_replace($id, $block_content, $split_content[$lang]);
+				}
+			}
+		}
+		if(!empty($split_content)) {
+			foreach($languages as $lang) {
+				$this->duplicate_post_as_draft($post, $split_content[$lang], strtoupper($lang));
+			}
+			return true;
+		}
+		return false;
+	}
+
+	// internal helpers -------------------------------------------------------]
+
+	private function find_blocks(&$content) {
 		$blocks = parse_blocks($content);
-// zu_log($blocks);
+		$found_blocks = [];
 		foreach($blocks as $index => $block) {
 			$blockName = $block['blockName'];
 			// if 'blockName' is null, we are dealing with a 'pre-Gutenberg' post ('Classic editor' block)
@@ -23,57 +97,65 @@ trait zu_TranslateConverted {
 				}
 			}
 		}
-// zu_logc('Found Blocks', $post_id, $found_blocks);
-// zu_log_if($block['blockName'] == 'core/paragraph', $block_content, $block_raw_content, $block);
+// zu_logc('Found Blocks', $found_blocks);
+		return $found_blocks;
+	}
 
-		foreach($found_blocks as $id => $raw) {
+	private function get_tokens($blocks) {
+		$block_tokens = [];
+		$languages = [];
+		foreach($blocks as $id => $raw) {
 			$lang_blocks = qtranxf_split($raw);
-// zu_log($lang_blocks);
-			$languages = array_keys($lang_blocks);
 			$tokens = [];
 			foreach($lang_blocks as $lang => $lang_raw) {
 				$tokens[$lang]['headings'] = $this->split_headings($lang_raw);
 				$fixed_raw = str_replace($tokens[$lang]['headings'][0], '', $lang_raw);
-// zu_log($fixed_raw);
 				$tokens[$lang]['shortcodes'] = $this->split_shortcodes($fixed_raw);
 				$tokens[$lang]['text'] = $this->split_paragraphs($fixed_raw, $tokens[$lang]);
 			}
-
-			$headings = [];
-			$paragraphs = [];
-			foreach($languages as $lang) {
-				foreach($tokens[$lang]['headings'][1] as $index => $value) {
-					$headings[$index][$lang] = $value;
-				}
-				foreach($tokens[$lang]['text'] as $index => $value) {
-					$paragraphs[$index][$lang] = $value;
-				}
-			}
-			$content = $this->format_blocks($lang_blocks[$post_lang], [
-				'headings'			=> $tokens[$post_lang]['headings'][0],
-				'paragraphs'		=> array_column($paragraphs, $post_lang),
-				'shortcodes'		=> $tokens[$post_lang]['shortcodes'],
-				'headings_raw'		=> array_map('qtranxf_join_b', $headings),
-				'paragraphs_raw'	=> array_map('qtranxf_join_b', $paragraphs),
-			]);
+			$block_tokens[$id] = $tokens;
+			$block_languages = array_keys($lang_blocks);
+			if(count($block_languages) > count($languages)) $languages = $block_languages;
 		}
-
-// zu_log($content);
-		if(!empty($found_blocks)) {
-			$this->duplicate_post_as_draft($post, $content);
-			return true;
-		}
-		return false;
+// zu_logc('Found Tokens', $block_tokens, $languages);
+		return [$block_tokens, $languages];
 	}
 
+	private function replace_blocks_with_raw($content, $params) {
+		$without_raw = $params['without_raw'] ?? false;
+		foreach($params['headings'] as $index => $value) {
+			$heading_raw = $without_raw ? strip_tags($value) : ($params['headings_raw'][$index] ?? '');
+			$content = str_replace($value, $this->core_heading($heading_raw, $value), $content);
+		}
+		foreach($params['shortcodes'] as $index => $value) {
+			if($value['parent' !== -1]) continue;
+// zu_log($value);
+			$shortcode_tag = $value['tag'];
+			$shortcode_raw = sprintf('%s%s%s',
+				$value['content'],
+				$value['inner'] ?? '',
+				$value['closing'] ? "[/{$shortcode_tag}]" : '',
+			);
+// zu_log($shortcode_raw);
+			$content = str_replace($shortcode_raw, $this->core_shortcode($shortcode_raw), $content);
+		}
+		foreach($params['paragraphs'] as $index => $value) {
+			$paragraph_raw = $without_raw ? strip_tags($value) : ($params['paragraphs_raw'][$index] ?? '');
+			$content = str_replace($value, $this->core_paragraph($paragraph_raw, $value), $content);
+		}
+		return $content;
+	}
+
+	// parse and analyse block contents ---------------------------------------]
+
 	private function split_headings($raw) {
-		$re = '/<h[1-6]+>([^<]+?)<\/h[1-6]+>/im'; // [\n]*
+		$re = '/<h[1-6]+>([^<]+?)<\/h[1-6]+>/im';
 		$result = preg_match_all($re, $raw, $matches, PREG_SET_ORDER, 0);
 		return $result ? [array_column($matches, 0), array_column($matches, 1)] : [[], []];
 	}
 
 	private function split_shortcodes($raw) {
-		$re = '/\[([\/]*[^\s|\]|\/]+)([^]]*)\]/m'; // [\n]*
+		$re = '/\[([\/]*[^\s|\]|\/]+)([^]]*)\]/m';
 		$result = preg_match_all($re, $raw, $matches, PREG_SET_ORDER, 0);
 		$shortcodes = [];
 		if($result) {
@@ -115,9 +197,7 @@ trait zu_TranslateConverted {
 	private function split_paragraphs($raw) {
 		$re = '/\[[^\]]+\][\n]*/m';
 		$without_shortcodes = strip_tags(preg_replace($re, '', $raw));
-	// zu_log($without_shortcodes);
 		$paragraphs = preg_split('/\n/', $without_shortcodes, -1, PREG_SPLIT_NO_EMPTY);
-	// zu_log($paragraphs);
 		return $paragraphs;
 	}
 
@@ -133,29 +213,7 @@ trait zu_TranslateConverted {
 		return false;
 	}
 
-	private function format_blocks($content, $params) {
-		foreach($params['headings'] as $index => $value) {
-			$heading_raw = $params['headings_raw'][$index] ?? '';
-			$content = str_replace($value, $this->core_heading($heading_raw, $value), $content);
-		}
-		foreach($params['shortcodes'] as $index => $value) {
-			if($value['parent' !== -1]) continue;
-// zu_log($value);
-			$shortcode_tag = $value['tag'];
-			$shortcode_raw = sprintf('%s%s%s',
-				$value['content'],
-				$value['inner'] ?? '',
-				$value['closing'] ? "[/{$shortcode_tag}]" : '',
-			);
-// zu_log($shortcode_raw);
-			$content = str_replace($shortcode_raw, $this->core_shortcode($shortcode_raw), $content);
-		}
-		foreach($params['paragraphs'] as $index => $value) {
-			$paragraph_raw = $params['paragraphs_raw'][$index] ?? '';
-			$content = str_replace($value, $this->core_paragraph($paragraph_raw, $value), $content);
-		}
-		return $content;
-	}
+	// format content as Gutenberg blocks -------------------------------------]
 
 	private function core_heading($raw, $rendered) {
 		$without_tags = strip_tags($rendered);
